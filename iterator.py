1d6f9bc0ff9263b98a70a1d90e33128af3a27cb7
CreatorXperience <allyearmustobey@gmail.com>
2024-04-13 10:55:59 +0000

    complete class implementation

--- iterator.py
+++ iterator.py
@@ -1,3 +1,4 @@
+import random
 tup = ("peter", "paul", "shark")
 iterator = iter(tup)
 
@@ -27,10 +28,10 @@ class ForEach:
 loo = ForEach()
 iterator = iter(loo)
 
-for x in iterator:
-    if x == 7 : 
-        break
-    print(x)
+# for x in iterator:
+    # if x == 7 : 
+    #     break
+    # print(x)
 
 
 
@@ -38,6 +39,7 @@ for x in iterator:
 
 
 class Parent: 
+
     def __init__(self, name, age):
         self.name =  name
         self.age = age
@@ -58,10 +60,77 @@ class Child(Parent):
         self.talk()
     
     def __str__(self) -> str:
-        return super().__str__()
+        return f"{self.talk}"
 
 
 human =  Child("emma", 10)
-print(human)
+# print(human)
 
 # num:int = "hi"
+
+
+class Bird:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+   
+    def __str__(self)-> str:
+        return f"{self.name} is a bird"
+   
+    def reproduce(self):
+        print(f"{self.name} can reproduce")
+
+
+class Eagle(Bird):
+
+    def __init__(self, name,color,speed):
+        super().__init__(name,color)
+        self.speed = speed
+
+    def fly(self):
+        print(f"{self.name} can fly")
+
+    def dive(self):
+        print(f"{self.name} can dive")
+    
+    def prey(self):
+        print(f"{self.name} can prey")
+
+
+# bird  = Bird("Eagle", "Black")
+eagle  = Eagle("Eagle", "Black", "Fast")
+eagle.reproduce()
+
+
+
+class iterand: 
+    def __init__(self, item:list[int]):
+        self.item = item
+
+    def __iter__(self):
+        self.item = self.item
+        return self
+    
+    def __next__(self):
+        length = len(self.item)
+        if length > 3 : 
+            n  = random.randint(0, length-1)
+            return [self.item[n]]
+
+        raise StopIteration
+
+loop = iterand([1,2])
+iterator = iter(loop)
+print(next(iterator))
+
+
+
+
+try: 
+    i = 0
+
+except: 
+    print("ma")   
+    
