6c2e3fa8c4cb8fd58378bf1f85e3cddf88037371
CreatorXperience <allyearmustobey@gmail.com>
2024-05-15 03:01:56 +0000

    python  re library

--- extlib.py
+++ extlib.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import sys
+import re
 print(os.getcwd())
 # print(os.chdir("/etc/"))
 print(os.getcwd())
@@ -16,4 +17,14 @@ print(sys.argv)
 shutil.copy("./lib.py", "./extlib.py")
 # shutil.move("./java.json", "./sec")
 sys.stderr.write("error occured in lib.py \n")
+
+print(re.findall(r"pe\w+", "peter peter pepper penguin pat zarticly particles"))
+recompiler = re.compile(r"\w+")
+print(recompiler.search("anime"))
+
+regez=re.finditer(r"\w+\@","me@ you@ life@")
+resub = re.sub(r"\@","$","peter@ habeeb@")
+print(resub)
+print(next(regez))
+print(next(regez))
 sys.exit(0)
